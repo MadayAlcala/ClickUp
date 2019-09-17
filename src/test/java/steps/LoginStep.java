10359baa56ec2f34fa6cc959d717f5c9e1093e42
Maday Alcala <Maday.Alcala@fundacion-jala.org>
2019-09-17 17:34:37 -0400

    Initial project core.

---
+++ src/test/java/steps/LoginStep.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+
+package steps;
+
+/**
+ * LoginStep class.
+ */
+public class LoginStep {
+}
