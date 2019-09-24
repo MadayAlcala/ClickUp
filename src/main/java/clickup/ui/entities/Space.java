b99fcabf61e13649ccf18fa1dae33b32869ce5ae
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-09-24 10:27:15 -0400

    Create for save some variable.

---
+++ src/main/java/clickup/ui/entities/Space.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+
+package clickup.ui.entities;
+
+/**
+ * Context class.
+ *
+ * @author Jesus Menacho
+ * @version 1.0
+ */
+public class Space {
+    private String title;
+
+    /**
+     * Method to let get the title.
+     * @return title type string.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Method to let set the title.
+     * @param title type string.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
