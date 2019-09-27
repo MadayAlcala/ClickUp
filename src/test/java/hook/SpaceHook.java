05e8632b94bf02c284647763844c9d3919ae9620
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-09-27 16:27:10 -0400

    Add new line, that let take a screen shot.

--- src/test/java/hook/SpaceHook.java
+++ src/test/java/hook/SpaceHook.java
@@ -12,9 +12,14 @@ package hook;
 
 import clickup.ui.entities.Context;
 import clickup.ui.pages.SpacePage;
+import core.selenium.WebDriverManager;
 import core.utils.Log;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import org.apache.log4j.spi.ErrorCode;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Contains after actions for do in a account.
@@ -24,7 +29,7 @@ import org.apache.log4j.spi.ErrorCode;
  */
 public class SpaceHook {
     private Context context;
-
+    private WebDriver webDriver;
     /**
      * Allows to receive the variable context.
      *
@@ -32,18 +37,21 @@ public class SpaceHook {
      */
     public SpaceHook(final Context context) {
         this.context = context;
+        this.webDriver = WebDriverManager.getInstance().getWebDriver();
     }
 
     /**
      * Lets logout from to web page.
      */
     @After(order = 1, value = "@logout")
-    public void logout() {
+    public void logout(Scenario scenario) {
         try {
             SpacePage starPage = new SpacePage();
             starPage.logOut();
         }
         catch (Exception ex){
+            byte[] screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
             Log.getInstance().getLog().error(ex);
             throw new NullPointerException("File not found" + ex);
         }
