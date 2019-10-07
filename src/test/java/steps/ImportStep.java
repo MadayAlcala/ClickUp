a297551474edacf42b94bf6d009ee69304d68a67
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-10-07 03:32:43 -0400

    Implement assertion.

--- src/test/java/steps/ImportStep.java
+++ src/test/java/steps/ImportStep.java
@@ -1,14 +1,24 @@
 package steps;
 
 import clickup.ui.pages.ImportPage;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.testng.Assert;
 
 public class ImportStep {
     private ImportPage importPage;
 
-    @When("The user access to import and fill fields")
+    public ImportStep() {
+        importPage = new ImportPage();;
+    }
+
+    @When("the user go to import and import CSV file")
     public void ImportFillFields() {
-        importPage = new ImportPage();
         importPage.importTable();
+    }
+
+    @Then("the import is succesfully")
+    public void theImportIsSuccesfully() {
+        Assert.assertEquals(true,importPage.isSuccessfullyImport());
     }
 }
