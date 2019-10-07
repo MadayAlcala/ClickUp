c00ddbc0859bdb42516e4bb311495f913d2901ac
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-10-07 03:28:34 -0400

    Implement import.

---
+++ src/main/java/clickup/entities/Import.java
@@ -0,0 +1,13 @@
+package clickup.entities;
+
+public class Import {
+    private Boolean importIsOk;
+
+    public Boolean getImportIsOk() {
+        return importIsOk;
+    }
+
+    public void setImportIsOk(Boolean importIsOk) {
+        this.importIsOk = importIsOk;
+    }
+}
