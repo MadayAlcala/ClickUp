a303ec738579682c198c4cf0449ab285db84962d
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-10-13 17:00:25 -0400

    Add context.

--- src/test/java/hook/ImportHook.java
+++ src/test/java/hook/ImportHook.java
@@ -1,14 +1,20 @@
 package hook;
 
+import clickup.entities.Context;
 import clickup.ui.pages.ApplicationPage;
 import clickup.ui.pages.ImportPage;
 import cucumber.api.java.After;
 
 public class ImportHook {
+    private Context context;
+
+    public ImportHook(Context context) {
+        this.context = context;
+    }
 
     @After("@deleteImport")
     public void deleteImport(){
-        ImportPage importPage=new ImportPage();
+        ImportPage importPage=new ImportPage(context);
         importPage.deleImportedTask();
     }
 }
