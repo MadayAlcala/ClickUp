456dca2efd8169aeb48f977a9a97c4c353c59c9a
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-10-13 17:01:16 -0400

    Refactor steps.

--- src/test/java/steps/ImportStep.java
+++ src/test/java/steps/ImportStep.java
@@ -1,15 +1,18 @@
 package steps;
 
+import clickup.entities.Context;
 import clickup.ui.pages.ImportPage;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.testng.Assert;
 
 public class ImportStep {
+    private Context context;
     private ImportPage importPage;
 
-    public ImportStep() {
-        importPage = new ImportPage();
+    public ImportStep(Context context) {
+        this.context=context;
+        importPage = new ImportPage(this.context);
     }
 
     @When("the user imports CSV file {string}")
