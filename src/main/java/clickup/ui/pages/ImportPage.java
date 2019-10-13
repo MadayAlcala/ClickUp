b96ea0c09e440bc697f7b2f49eaa3ab0bd65b3d9
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-10-13 17:00:02 -0400

    Add new funcionality.

--- src/main/java/clickup/ui/pages/ImportPage.java
+++ src/main/java/clickup/ui/pages/ImportPage.java
@@ -1,15 +1,22 @@
 package clickup.ui.pages;
 
+import clickup.entities.Context;
 import clickup.ui.BasePage;
 import core.utils.Log;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class ImportPage extends BasePage {
+    private Context context;
     private Boolean isSuccessfully;
+    private static final String SPACE_MENU = "//div[@class='user-list-item ng-star-inserted']//div[./text()='%s']";
     @FindBy(css = ".cu-avatar-container")
     private WebElement spaceBarButton2;
 
@@ -34,8 +41,8 @@ public class ImportPage extends BasePage {
     @FindBy(css = ".cu-btn.ng-star-inserted")
     private WebElement selectSpaceButton;
 
-    @FindBy(xpath = "//div[contains(@class,'user-list-item__name')][contains(.,'ads')]")
-    private WebElement selectSpaceMenu;
+//    @FindBy(xpath = "//div[contains(@class,'user-list-item__name')][contains(.,'ads')]")
+//    private WebElement selectSpaceMenu;
 
     @FindBy(css = ".cu-nav-sts__back")
     private WebElement backButton;
@@ -67,6 +74,23 @@ public class ImportPage extends BasePage {
     @FindBy(xpath = "//*[contains(text(), 'pepito')]")
     private WebElement task1;
 
+    @FindBy(xpath = "//*[contains(text(), 'Next')]")
+    private WebElement continueButtonImport;
+
+    @FindBy(xpath = "//div[contains(@class,'cu-settings-section cu-settings-section_open ng-star-inserted')]//*[contains(text(), 'Next')]")
+    private WebElement continueButtonImport2;
+
+    @FindBy(xpath = "//button[contains(@class,'cu-btn')][contains(text(), 'Start Import')]")
+    private WebElement startImportButton;
+
+    /**
+     * Constructor.
+     * @param context parameter.
+     */
+    public ImportPage(Context context) {
+        this.context = context;
+    }
+
     public void importTable(String fileImport) {
         getWait().until(ExpectedConditions.elementToBeClickable(spaceBarButton2));
         spaceBarButton2.click();
@@ -90,11 +114,28 @@ public class ImportPage extends BasePage {
             e.printStackTrace();
         }
         robot.delay(500);
-        selectSpaceMenu.click();
+        getDriver().findElement(By.xpath(String.format(SPACE_MENU,context.getSpace().getTitle()))).click();
+        continueButtonImport.click();
+        robot.delay(50);
+        robot.keyPress(KeyEvent.VK_END);
+        robot.keyRelease(KeyEvent.VK_END);
+        robot.delay(50);
+        continueButtonImport2.click();
+        startImportButton.click();
         pastImportButton.click();
         imputProgressButton.click();
         isSuccessfully = imputProgressButton.isEnabled();
         backButton.click();
+    }
+
+    /**
+     * Returns a space webElement.
+     *
+     * @param spaceName that is the name of the space to find.
+     * @return WebElement 'space'.
+     */
+    private WebElement getSpaceElementByName(final String spaceName) {
+        return getDriver().findElement(By.xpath(String.format(SPACE_MENU, spaceName)));
     }
 
     public boolean isSuccessfullyImport() {
